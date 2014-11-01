e0a14458fd66ed43cbf55001a0387848405d898c
INADA Naoki <inada-n@klab.com>
2014-11-02 06:49:58 +0900

    Fix example/hello_world

--- example/hello_world.py
+++ example/hello_world.py
@@ -1,13 +1,19 @@
-from meinheld import server
+from minefield import server
+import time
+
 
 def hello_world(environ, start_response):
     status = '200 OK'
     res = b"Hello world!"
     response_headers = [('Content-type','text/plain')]
     start_response(status, response_headers)
-    #print(environ)
+    print(environ)
     return [res]
 
+def watchdog():
+    print(time.time())
+
+server.set_watchdog(watchdog)
 server.listen(("0.0.0.0", 8000))
 server.set_access_logger(None)
 server.set_error_logger(None)
